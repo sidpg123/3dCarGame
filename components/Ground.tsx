89d13a0cf51e253b6240af545f585a74916f7574
Siddharth <preparation013@gmail.com>
2024-09-06 11:17:19 +0530

    resolved ref error

--- components/Ground.tsx
+++ components/Ground.tsx
@@ -1,7 +1,7 @@
 import { usePlane } from "@react-three/cannon";
-
+import * as THREE from "three"
 const Ground: React.FC = () => {
-  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0] }));
+  const [ref] = usePlane<THREE.Mesh>(() => ({ rotation: [-Math.PI / 2, 0, 0] }));
   return (
     <mesh ref={ref} receiveShadow name="ground">
       <planeGeometry args={[100, 100]} />
